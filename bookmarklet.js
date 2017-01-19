35d029a81a29600ea9eca59d46f54eed9d687403
Peter L <peterapps@users.noreply.github.com>
2017-01-19 08:18:45 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,18 +1,10 @@
-// Feross Aboukhadijeh - Apr 12 2010
-// 
-// Script I hacked together to cheat on TypeRacer.com. You use it by waiting for the typing game
-// to start. Once it starts, open up Firebug, paste in this code, and run it. Now, just press
-// space to auto-type each word. I made the user push space, as opposed to advancing the words 
-// automatically because I believe the site looks for a keypress event before evaluating the contents
-// of the input box. I could not figure out how to fake a user keypress event. Perhaps this is
-// disallowed for browser security reasons?
-//
-// Next todo: Site detects the unbelievable WPM and asks you to do a captcha test.
-
-wordNumber = 0;
-function PrintNextWord() {
-    word = answerArr[wordNumber];
-    inputs[0].value = word + " ";
+javascript:
+(function(){
+    function oldA(){
+        return document.getElementById("nhwMiddlegwt-uid-8")||document.getElementById("nhwMiddlegwt-uid-16");
+    };
+    function a() {
+    return answerArr[wordNumber];
     wordNumber++;
 }
 
@@ -36,10 +28,10 @@ for (i=0; i<99; ++i) {
     }
 }
 
-answer = firstWord.innerHTML + answer.innerHTML;
-// alert(answer);
-answerArr = answer.split(" ");
-
-inputs = document.getElementsByTagName("input");
-
-PrintNextWord();
+    var b=document.getElementsByClassName("txtInput")[0];
+    document.addEventListener("keypress",function(e){
+        if(e.keyCode == 32){
+            b.value=a().innerHTML;
+        }
+    });
+})();
